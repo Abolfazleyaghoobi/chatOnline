fee6c19bbad8deffd6f52baa4330b4add4446e97
Abolfazle <80malard57@gmail.com>
2026-05-27 23:23:39 -0700

    Add ChatBox component with styles and input functionality

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,10 +1,29 @@
 import "./ChatBox.css";
+import assets from "../../assets/assets";
 function ChatBox() {
     return ( 
         <>
-            <h2>ChatBox</h2>
+          <div className="chat-box">
+            <div className="chat-user">
+                <img src={assets.profile_img} alt="" />
+                <p>Abolfazle <img className="dot" src={assets.green_dot} alt="" /></p>
+                <img src={assets.help_icon} className="help" alt="" />
+            </div>
+            <div className="chat-input">
+                <input type="text" placeholder="send a message" />
+                <input type="file" name="" id="image"  accept="image/png,image/jpeg" hidden/>
+                <label htmlFor="image">
+                    {/* choose icon gallery */}
+                    <img src={assets.gallery_icon} alt="" />
+                </label>
+                {/* send icon */}
+                <img src={assets.send_button} alt="" />
+
+            </div>
+          </div>
         </>
      );
+     
 }
 
 export default ChatBox;
