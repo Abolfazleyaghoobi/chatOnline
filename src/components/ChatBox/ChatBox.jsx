7a7c9b0ce830cf604f5173ec9133996b72e2f314
Abolfazle <80malard57@gmail.com>
2026-05-29 18:02:57 -0700

    Add chat message display and styling to ChatBox component

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -3,12 +3,52 @@ import assets from "../../assets/assets";
 function ChatBox() {
     return ( 
         <>
+        {/* User profile */}
           <div className="chat-box">
             <div className="chat-user">
                 <img src={assets.profile_img} alt="" />
                 <p>Abolfazle <img className="dot" src={assets.green_dot} alt="" /></p>
                 <img src={assets.help_icon} className="help" alt="" />
             </div>
+
+            {/* chat  message */}
+
+                <div className="chat-msg">
+                    {/* message sender */}
+                    <div className="s-msg">
+                      <img src={assets.pic1}
+                      className="msg-img" alt="" />
+                            <div>
+                                <img src={assets.profile_img} alt="" />
+                                <p>2:30 PM</p>
+                            </div>
+                    </div>
+                    <div className="s-msg">
+                            <p className="msg">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                            <div>
+                                <img src={assets.profile_img} alt="" />
+                                <p>2:30 PM</p>
+                            </div>
+                    </div>
+                    {/* resive message */}
+                    <div className="r-msg">
+                            <p className="msg">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                            <div>
+                                <img src={assets.profile_img} alt="" />
+                                <p>2:30 PM</p>
+                            </div>
+                    </div>
+
+
+                </div>
+
+
+
+
+
+
+
+            {/* unput chat */}
             <div className="chat-input">
                 <input type="text" placeholder="send a message" />
                 <input type="file" name="" id="image"  accept="image/png,image/jpeg" hidden/>
