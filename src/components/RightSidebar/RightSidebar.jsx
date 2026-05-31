60063f99ed1b5ff845f65c615bdbbc0936b5a51e
Abolfazle <80malard57@gmail.com>
2026-05-30 22:08:38 -0700

    Add RightSidebar component with profile and media display

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,7 +1,28 @@
 import "./RightSidebar.css";
+import assets from "../../assets/assets"
 function RightSidebar() {
     return (  <>
-    <h2>Right Sidebar</h2>
+            <div className="rs">
+                <div className="rs-profile">
+                    <img src={assets.profile_img} alt="" />
+                    <h3>Abolfazle <img src={assets.green_dot} className="dot" alt="" /></h3>
+                    <p>Lorem ipsum dolor sit amet.</p>
+
+                </div>
+                <hr />
+                <div className="rs-media">
+                    <p>media</p>
+                    <div>
+                        <img src={assets.pic1} alt="" />
+                        <img src={assets.pic2} alt="" />
+                        <img src={assets.pic3} alt="" />
+                        <img src={assets.pic4} alt="" />
+                        <img src={assets.pic1} alt="" />
+                        <img src={assets.pic1} alt="" />
+                    </div>
+                </div>
+                <button>logout</button>
+            </div>
     </>);
 }
 export default RightSidebar;
