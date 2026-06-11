b1d5edd49d892d2e0ef3034b216d5be5b61c8d6c
Abolfazle <80malard57@gmail.com>
2026-06-10 19:11:47 -0700

    Add submenu for profile editing and logout in LeftSidebar component

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,6 +1,8 @@
 import "./LeftSidebar.css";
 import assets from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 function LeftSidebar() {
+  const navigate=useNavigate();
   return (
     <div className="ls">
       <div className="ls-top">
@@ -9,6 +11,12 @@ function LeftSidebar() {
           <img src={assets.logo} alt="" className="logo" />
           <div className="menu ">
             <img src={assets.menu_icon} alt="" />
+            <div className="subMenu">
+              <p >Edite profile</p>
+              <hr />
+              <p >Logout</p>
+
+            </div>
           </div>
         </div>
         {/* search bar */}
